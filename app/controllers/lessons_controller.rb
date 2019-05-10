b004b13d136af006ff70953d2785efaf4409ae96
Rajaomalala <rajaomalalasendrampanjato@gmail.com>
2019-05-11 01:27:42 +0300

    final reaction for today

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,10 @@
+class LessonsController < ApplicationController
+  def index
+  end
+  def one
+  end
+  def two
+  end
+  def three
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  
+  get 'lessons/index'
   root "lessons#index"
-  namespace :admins do
-    end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'lessons/1', to: "lessons#one"
+  get 'lessons/2', to: "lessons#two"
+  get 'lessons/3', to: "lessons#three"
 end
