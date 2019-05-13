333650245a97c8955ef1e3797bfc105e84e4800b
Rajaomalala <rajaomalalasendrampanjato@gmail.com>
2019-05-13 10:02:32 +0300

    the modification inside of our app anglisy

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -6,9 +6,13 @@ class LessonsController < ApplicationController
   def two
   end
   def three
-  	@lessons_malagasy_k = ["Antsy", "Mahafantatra"]
-  	@lessons_anglisy_k = ["Knife", "Know"] 
-  	@lessons_anglisy_m = []
-  	@lessons_anglisy_b = []
+  	@lessons_malagasy_k = ["Antsy", "Mahafantatra", "Lohalika/Mandohalika", "Mandohalika", "Mandondona"]
+  	@lessons_anglisy_k = ["Knife", "Know", "Knee", "Kneel", "Knock"] 
+  	@lessons_anglisy_n = ["Hymn", "Damn", "Solemn", "Condemn", "Column"]
+  	@lessons_malagasy_n = ["Hira", "Manozona", "Manetriketrika", "Manameloka", "\"Colonne\""]
+  	@lessons_anglisy_b = ["Climb", "Comb", "Crumb", "Debt", "Thumb", "Tomb", "Womb"]
+  	@lessons_malagasy_b = ["Mihanika", "Mibango", "Potipotiny", "Trosa", "Ankihibe", "Fasana", "Tranon-jaza"]
+  	@lessons_anglisy_p = ["Psalm",  "Psychology", "Psychiatry", "Pseudo"]
+  	@lessons_malagasy_p = ["Salamo", "\"Psychologie\"", "\"Psychiatrie\"", "\"Pseudo\""]
   end
 end
